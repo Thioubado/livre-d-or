27ee817d1140a901930bf2e2b338d836878d00f0
Unknown <seckndanane16@gmail.com>
2019-04-20 03:39:31 +0100

    Erreur au niveau de la connexion à la base de données

---
+++ blog/index.php
@@ -0,0 +1,11 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', 'fatimata16');
+require_once '../elements/header.php';
+echo 'hello';
+?>
+
+
+
+
+<?php require_once '../elements/footer.php';
+
