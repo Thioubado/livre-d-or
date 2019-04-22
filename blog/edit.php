ce7149c30092ef99817a3a657f0568d63ed3e0f2
Unknown <seckndanane16@gmail.com>
2019-04-22 01:28:52 +0100

    première partie du blog

---
+++ blog/edit.php
@@ -0,0 +1,58 @@
+<?php
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=blog', 'root', '',[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+]);
+$error = null;
+$success = null;
+try {
+    if(isset($_POST['name'], $_POST['content'])){
+        $query = $pdo->prepare('UPDATE posts SET name = :name, content = :content WHERE id = :id');
+        $query-> execute([
+            'name' => $_POST['name'],
+            'content' => $_POST['content'],
+            'id' => $_GET['id']
+        ]);
+        $succes = 'Votre article a bien été modifié';
+    }
+    $query = $pdo -> prepare('SELECT * FROM posts WHERE id= :id');
+    $query->execute([
+        'id' => $_GET['id']
+    ]);
+
+$post = $query ->Fetch();
+
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+}
+
+require_once '../elements/header.php';
+?>
+    <div class="container">
+    <?php if($succes) : ?>
+
+        <div class="alert alert-success">
+            <?= $succes ?>
+        </div>
+
+    <?php endif ?>
+    <?php if($error): ?>
+        <div class="alert alert-danger">
+            <?= $error;?>
+        </div>
+    <?php else : ?>
+
+        <form action="" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="name" value="<?= htmlentities($post->name) ?>">
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" name="content" value=""><?= htmlentities($post->content) ?></textarea>
+            </div>
+            <button class="btn btn-primary">Sauvegarder</button>
+        </form>
+        
+    <?php endif ?>
+    </div>
+
+<?php require_once '../elements/footer.php';
--- blog/index.php
+++ blog/index.php
@@ -1,11 +1,34 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', 'fatimata16');
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=blog', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$error = null;
+try {
+    $query = $pdo -> query('SELECT * FROM posts');
+//if($query === false){
+    //var_dump($pdo->errorInfo());
+    //die('Erreur SQL');
+//}
+$posts = $query ->FetchAll(PDO::FETCH_OBJ);
+/*echo'<pre>';
+print_r($posts);
+echo '</pre>';*/
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+}
+
 require_once '../elements/header.php';
-echo 'hello';
 ?>
+    <?php if($error): ?>
+        <div class="alert alert-danger">
+            <?= $error;?>
+        </div>
+    <?php else : ?>
 
-
-
+        <ul>
+            <?php foreach($posts as $post) : ?>
+                <li><a href="/livre-d-or/blog/edit.php?id=<?= $post->id ?>"><?= $post->name ;?></a></li>
+            <?php endforeach ?>
+        </ul>
+    <?php endif ?>
 
 <?php require_once '../elements/footer.php';
-
